5251d350d9a33491025405bf4054bdc0aa7a7834
Eduardo <eduardo.gregorio.ferrer@gmail.com>
2026-03-14 19:00:46 +0100

    añadido firebase para tocar tema de ddbb, y tocado una cosa de una version para que dejase firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+// imports de firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MyApp());
 }
 
