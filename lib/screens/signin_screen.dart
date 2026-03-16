68884992173c24180f9ece0113e80156ccbeea3d
Eduardo <eduardo.gregorio.ferrer@gmail.com>
2026-03-16 18:36:42 +0100

    validaciones cuando te registras

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tfg/screens/login_screen.dart';
 import 'package:tfg/screens/main_screen.dart';
 
-import 'map_screen.dart';
-
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
 
@@ -12,7 +10,33 @@ class SigninScreen extends StatefulWidget {
 }
 
 class _SigninScreenState extends State<SigninScreen> {
+  
+  final TextEditingController _passwordController = TextEditingController();
+  
   bool _acceptTerms = false;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _birthDateController = TextEditingController();
+
+  @override
+  void dispose() {  // Borra el controller cuando se cierre el calendario, asi no consume memoria
+    _birthDateController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now().subtract(const Duration(days: 6570)), // ~18 años atrás
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _birthDateController.text = "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +46,8 @@ class _SigninScreenState extends State<SigninScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Center(
+        child: Form(
+          key: _formKey,
           child: SingleChildScrollView(
             child: Card(
               elevation: 4,
@@ -42,37 +67,93 @@ class _SigninScreenState extends State<SigninScreen> {
                       label: const Text('con google'),
                     ),
                     const SizedBox(height: 20),
-                    const TextField(
-                      decoration: InputDecoration(
+                    TextFormField(
+                      validator: (value) {
+                        // Abarcamos todas las posibilidades para que sea un correo
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, introduce un correo';
+                        }
+                        final emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'); //esto indica la estructura de un correo
+                        if (!emailRegExp.hasMatch(value)) {
+                          return 'Introduce un correo electrónico válido';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
                         labelText: 'Correo electrónico',
                         border: OutlineInputBorder(),
+                        hintText: 'ejemplo@correo.com',
                       ),
                     ),
                     const SizedBox(height: 15),
-                    const TextField(
-                      decoration: InputDecoration(
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, introduce un usuario';
+                        } else if (value.length < 3) {
+                          return 'El usuario debe tener al menos 3 caracteres';
+                        } else if (value.length > 15) {
+                          return 'El usuario no puede tener más de 15 caracteres';
+                        } else if (!RegExp(r'^[a-zA-Z0-9]+$').hasMatch(value)) { // evita espacios, puntos, comas, guiones o emojis en un nombre de usuario
+                          return 'El usuario solo puede contener letras y números';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
                         labelText: 'Usuario',
                         border: OutlineInputBorder(),
                       ),
                     ),
                     const SizedBox(height: 15),
-                    const TextField(
+                    TextFormField(
+                      controller: _passwordController,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, introduce una contraseña';
+                        } else if (value.length < 8) {
+                          return 'La contraseña debe tener al menos 8 caracteres';
+                        } else if (!RegExp(r'[a-zA-Z]').hasMatch(value)) {
+                          return 'La contraseña debe contener al menos una letra';
+                        } else if (!RegExp(r'[0-9]').hasMatch(value)) {
+                          return 'La contraseña debe contener al menos un número';
+                        }
+                        return null;
+                      },
                       obscureText: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: 'Contraseña',
                         border: OutlineInputBorder(),
                       ),
                     ),
-                    const TextField(
+                    const SizedBox(height: 15),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Por favor, repite la contraseña';
+                        } else if (value != _passwordController.text) {
+                          return 'Las contraseñas no coinciden';
+                        }
+                        return null;
+                      },
                       obscureText: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: 'Repetir Contraseña',
                         border: OutlineInputBorder(),
                       ),
                     ),
                     const SizedBox(height: 15),
-                    const TextField(
-                      decoration: InputDecoration(
+                    TextFormField(
+                      controller: _birthDateController,
+                      readOnly: true,
+                      onTap: () => _selectDate(context),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Selecciona tu fecha de nacimiento';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
                         labelText: 'Fecha de nacimiento',
                         border: OutlineInputBorder(),
                         suffixIcon: Icon(Icons.calendar_today),
@@ -97,11 +178,19 @@ class _SigninScreenState extends State<SigninScreen> {
                     const SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => const MainScreen()),
-                              (route) => false,
-                        );
+                        if (_formKey.currentState!.validate()) {
+                          if (!_acceptTerms) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(content: Text('Debes aceptar los términos y condiciones')),
+                            );
+                            return;
+                          }
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(builder: (context) => const MainScreen()),
+                            (route) => false,
+                          );
+                        }
                       },
                       child: const Text('Registrarse'),
                     ),
@@ -111,7 +200,7 @@ class _SigninScreenState extends State<SigninScreen> {
                         Navigator.pushAndRemoveUntil(
                           context,
                           MaterialPageRoute(builder: (context) => const LoginScreen()),
-                              (route) => false,
+                          (route) => false,
                         );
                       },
                       child: const Text('Cancelar'),
